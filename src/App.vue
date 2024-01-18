2dca3dd080f6caca776eef3f847bab77fe3a3f12
JulienZeng <936636589@qq.com>
2024-01-18 14:27:54 +0800

    添加备案号并调整样式

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,15 @@ const str = ref();
 
 const copy = () => {
   navigator.clipboard.writeText(str.value);
+};
 
+const ICPInfo = {
+  icpNo: "粤ICP备2022014957号-1",
+  psbNo: "粤公网安备 44049102496850号",
+  psbLogoURL: "https://beian.mps.gov.cn/img/logo01.dd7ff50e.png",
+  icpRecoadURL: "https://beian.miit.gov.cn/",
+  psbRecoadURL:
+    "http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44049102496850",
 };
 
 const getKFC = () => {
@@ -24,7 +32,7 @@ getKFC();
 </script>
 
 <template>
-  <div>
+  <div style="flex: 1;">
     <div class="title">
       <h1>KFC Crazy Thursday 文案</h1>
     </div>
@@ -34,6 +42,15 @@ getKFC();
     <div class="btn">
       <div class="copy" @click="copy">复制</div>
       <div class="reload" @click="getKFC()">刷新</div>
+    </div>
+  </div>
+  <div class="footer">
+    <div class="icp">
+      <a :href="ICPInfo.icpRecoadURL">{{ ICPInfo.icpNo }}</a>
+    </div>
+    <div class="psb">
+      <img class="psb-logo" :src="ICPInfo.psbLogoURL" />
+      <a :href="ICPInfo.psbRecoadURL">{{ ICPInfo.psbNo }}</a>
     </div>
   </div>
 </template>
@@ -63,5 +80,27 @@ getKFC();
   display: flex;
   flex-direction: row;
   justify-content: end;
+}
+.footer {
+  display: flex;
+  flex-direction: column;
+  margin: 20px 0 20px 0;
+}
+.icp {
+  margin: 10px 0 10px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.psb {
+  margin: 10px 0 10px 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+.psb-logo {
+  width: 20px;
+  height: 20px;
 }
 </style>
